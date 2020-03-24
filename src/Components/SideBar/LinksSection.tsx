275f3a3c0ea45bd8e8eaeb018c438f0feeb08c20
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-24 12:07:37 +0000

    add seperate links section component

---
+++ src/Components/SideBar/LinksSection.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import { navigate } from "@reach/router";
+import { MailOption, Twitter, Instagram } from "grommet-icons";
+
+const NavLinks = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  width: 75%;
+`;
+
+const NavItem = styled.li`
+  margin-top: 0.5em;
+`;
+
+const LinksSection = () => {
+  return (
+    <NavLinks>
+      <NavItem onClick={() => navigate("mailto:rj@richjames.co.uk")}>
+        <MailOption color="black" />
+      </NavItem>
+      <NavItem onClick={() => navigate("https://twitter.com/nowhitetee")}>
+        <Twitter color="black" />
+      </NavItem>
+      <NavItem onClick={() => navigate("https://instagram.com/nowhitetees")}>
+        <Instagram color="black" />
+      </NavItem>
+    </NavLinks>
+  );
+};
+
+export default LinksSection;
