b37654e45bf94f2d1f9d09924848850d8deff96f
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-24 21:58:35 +0000

    add progress container

--- src/Components/SideBar/ProgressIndicator.tsx
+++ src/Components/SideBar/ProgressIndicator.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import styled from "styled-components/macro";
+import { PlayFill, PauseFill } from "grommet-icons";
 
 const ProgressContainer = styled.div`
   border: 1px solid black;
   height: 100%;
-  //   width: 30%;
+  width: 30%;
   align-self: flex-start;
 `;
-
 const ProgressIndicator = () => {
-  return <ProgressContainer></ProgressContainer>;
+  return (
+    <ProgressContainer>
+      <div>
+        <PlayFill color="black" />
+      </div>
+      <div>
+        <PauseFill color="black" />
+      </div>
+    </ProgressContainer>
+  );
 };
 
 export default ProgressIndicator;
