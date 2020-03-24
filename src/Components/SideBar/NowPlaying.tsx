6b485c317c6e9c02a3a37312e711b16e64056499
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-24 12:07:48 +0000

    add now playing component

---
+++ src/Components/SideBar/NowPlaying.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const NowPlayingContainer = styled.div``;
+
+const NowPlaying = () => {
+  return (
+    <NowPlayingContainer>
+      <p>Now</p>
+    </NowPlayingContainer>
+  );
+};
+
+export default NowPlaying;
