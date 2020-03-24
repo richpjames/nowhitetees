28e025fb668f78f2e8d35f4b26d4fdb53b31ecaa
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-24 09:14:13 +0000

    remove routing for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from "react";
 import styled from "styled-components/macro";
-import { Router } from "@reach/router";
 
-import NotFound from "./NotFound";
 import "./App.css";
-import NavBar from "./Components/NavBar";
+import SideBar from "./Components/NavBar";
 import Home from "./Components/Home";
-import IndividualShow from "./Components/ShowReel/LongCard/IndividualShow";
 
-const AppWrap = styled.div``;
+const AppWrap = styled.div`
+  display: flex;
+`;
 
 function App() {
   return (
     <AppWrap className="AppWrap">
-      <NavBar />
-      <Router>
-        <Home path="/" />
-        <IndividualShow path="/:date" />
-        <NotFound default />
-      </Router>
+      <SideBar />
+      <Home path="/" />
     </AppWrap>
   );
 }
