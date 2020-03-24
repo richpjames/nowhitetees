d230d069dea3f5db054f856531818d49318959e7
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-24 15:41:51 +0000

    add progress indicator component

---
+++ src/Components/SideBar/ProgressIndicator.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const ProgressContainer = styled.div`
+  border: 1px solid black;
+  height: 100%;
+  //   width: 30%;
+  align-self: flex-start;
+`;
+
+const ProgressIndicator = () => {
+  return <ProgressContainer></ProgressContainer>;
+};
+
+export default ProgressIndicator;
