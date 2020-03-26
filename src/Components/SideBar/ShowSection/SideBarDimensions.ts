4cc212b80ef9908ed3a9eee0dd0f76059d986d91
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-26 12:37:39 +0000

    create sideBarDimensions defintions

---
+++ src/Components/SideBar/ShowSection/SideBarDimensions.ts
@@ -0,0 +1,10 @@
+export const height = 100;
+export const nowPlayingHeight = 15;
+export const buttonsHeight = 10;
+export const secondSectionHeight = height - (nowPlayingHeight + buttonsHeight);
+export const volumeHeight = 15;
+export const tracklistHeight = secondSectionHeight - volumeHeight;
+
+export const width = 100;
+export const progressWidth = 33.3;
+export const volTracklistWidth = width - progressWidth;
