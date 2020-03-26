9f7ebec2848f96a29ebc3eef61be2b86514fd9f4
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-26 12:46:15 +0000

    style correctly to fit in box

--- src/Components/SideBar/ShowSection/ProgressIndicator.tsx
+++ src/Components/SideBar/ShowSection/ProgressIndicator.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import styled from "styled-components/macro";
 import { useAudioPosition } from "react-use-audio-player";
+import { secondSectionHeight, progressWidth } from "./SideBarDimensions";
 
 const Container = styled.div`
-  align-self: flex-start;
-  border: 1px solid black;
-  height: 100%;
-  width: 100%;
+  width: ${progressWidth}%;
+  height: ${secondSectionHeight}%;
 `;
 
 const ProgressBar = styled.div``;
 
-const ProgressCircle = styled.div`
-  border: 1px solid green;
-  width: ;
-  height: 30px;
-`;
+const ProgressCircle = styled.div``;
 const ProgressIndicator = () => {
   const { position, duration } = useAudioPosition({ highRefreshRate: true });
   const [percent, setPercent] = React.useState(0);
