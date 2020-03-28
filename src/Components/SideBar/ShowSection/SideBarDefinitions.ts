3370ff1bf69272bfa98f6f0e1f6dc0ee8391b316
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-28 17:04:24 +0000

    update tracklistHeight

--- src/Components/SideBar/ShowSection/SideBarDefinitions.ts
+++ src/Components/SideBar/ShowSection/SideBarDefinitions.ts
@@ -5,7 +5,7 @@ export const nowPlayingHeight = 10;
 export const buttonsHeight = 10;
 export const secondSectionHeight = height - (nowPlayingHeight + buttonsHeight);
 export const volumeHeight = 15;
-export const tracklistHeight = secondSectionHeight - volumeHeight;
+export const tracklistHeight = secondSectionHeight;
 
 export const width = 100;
 export const progressWidth = 33.3;
