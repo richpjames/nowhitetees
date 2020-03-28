3f00f16e78f8f1f8857756867365f4c9a4600843
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-28 11:09:50 +0000

    remove howler

--- src/Components/SideBar/ShowSection/Volume.tsx
+++ src/Components/SideBar/ShowSection/Volume.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components/macro";
+// import { Howler } from "howler";
+
 import { SideBarContainer } from "./SideBarDefinitions";
+
+// out of use until react-use-audio-player exposes volume control
 
 const Container = styled(SideBarContainer)``;
 
@@ -8,7 +12,13 @@ const Volume = () => {
   return (
     <Container height={15} width={100}>
       <label htmlFor="volume">Volume</label>
-      <input type="range" min={0} max={100} id="volume" />
+      <input
+        type="range"
+        min={0}
+        max={1}
+        id="volume"
+        // onChange={({ target }) => Howler.volume(+target.value)}
+      />
     </Container>
   );
 };
