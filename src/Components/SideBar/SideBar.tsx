fcd2b99c2d27e7731fbf251821582cce20b21f71
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-28 17:18:42 +0000

    add blanker when no show is selected

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components/macro";
 import LogoCentered from "../LogoCentered";
 import LinksSection from "./LinksSection";
 import ShowSection from "./ShowSection/ShowSection";
+import { AppContext } from "../../AppContext";
 
 const Nav = styled.section`
   display: flex;
@@ -21,17 +22,31 @@ const Nav = styled.section`
 const LogoWrap = styled.div`
   height: 22.5%;
 `;
+const Blanker = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 70%;
+`;
 
 interface IProps {}
 
 const SideBar: React.FC<IProps> = (props: IProps) => {
+  const { showDate } = React.useContext(AppContext);
   return (
     <Nav>
       <LogoWrap>
         <LogoCentered height="150px" width="150px" className="Logo" />
       </LogoWrap>
       <LinksSection />
-      <ShowSection />
+      {showDate ? (
+        <ShowSection />
+      ) : (
+        <Blanker>
+          <h5>Select a show...</h5>
+        </Blanker>
+      )}
     </Nav>
   );
 };
