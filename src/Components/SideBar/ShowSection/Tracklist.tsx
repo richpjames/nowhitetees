5cad35482d19e45620439368d71392ace63f6bde
Richard James <rich@Richards-MacBook-Pro.local>
2020-03-28 17:03:40 +0000

    create tracklist component

--- src/Components/SideBar/ShowSection/Tracklist.tsx
+++ src/Components/SideBar/ShowSection/Tracklist.tsx
@@ -1,16 +1,45 @@
 import React from "react";
 import styled from "styled-components/macro";
 
-const Container = styled.div`
-  border: 1px solid black;
-  height: calc(100% - 2px);
-  width: calc(100% - 2px);
-`;
+import {
+  SideBarContainer,
+  tracklistHeight,
+  volTracklistWidth
+} from "./SideBarDefinitions";
 
-const Tracklist = () => {
+const Container = styled(SideBarContainer)`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+`;
+const PageTitle = styled.h4`
+  padding-top: 2.5%;
+  padding-bottom: 2.5%;
+`;
+const TracklistWrap = styled.div`
+  overflow-y: scroll;
+`;
+const Artist = styled.h5``;
+const TrackTitle = styled.p``;
+const Track = styled.div`
+  padding: 5px;
+`;
+interface IProps {
+  tracklist: Track[];
+}
+
+const Tracklist = (props: IProps) => {
+  const { tracklist } = props;
+  const tracklistElems = tracklist.map(track => (
+    <Track>
+      <Artist>{track.artist}</Artist>
+      <TrackTitle>{track.track}</TrackTitle>
+    </Track>
+  ));
   return (
-    <Container>
-      <h4>Tracklist:</h4>
+    <Container height={tracklistHeight} width={volTracklistWidth}>
+      <PageTitle>Tracklist:</PageTitle>
+      <TracklistWrap> {tracklistElems}</TracklistWrap>
     </Container>
   );
 };
