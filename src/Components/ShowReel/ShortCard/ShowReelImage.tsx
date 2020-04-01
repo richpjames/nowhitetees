77142027185ab7cd15b97798dfcf36934849e48e
Richard James <rich@Richards-MacBook-Pro.local>
2020-04-01 18:25:18 +0100

    pass onClick through

--- src/Components/ShowReel/ShortCard/ShowReelImage.tsx
+++ src/Components/ShowReel/ShortCard/ShowReelImage.tsx
@@ -5,6 +5,7 @@ import Image from "../../Image";
 interface Props {
   path: string;
   className?: string;
+  onClick?: () => void;
 }
 
 const ImageWrap = styled.div`
@@ -15,9 +16,9 @@ const ImageWrap = styled.div`
 const ShowReelImage: React.FC<Props> = (props: Props) => {
   const baseImgUrl = `https://www.nowhitetees.co.uk/img/homepage_thumbs/`;
 
-  const { path } = props;
+  const { path, onClick } = props;
   return (
-    <ImageWrap>
+    <ImageWrap onClick={onClick}>
       <Image src={`${baseImgUrl}${path}`} alt="show image" maxWidth="100%" />
     </ImageWrap>
   );
