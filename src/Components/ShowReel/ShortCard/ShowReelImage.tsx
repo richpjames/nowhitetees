0b9c96fad795b1c99a111daa643dd8497144c311
Richard James <rich@Richards-MacBook-Pro.local>
2020-04-09 10:15:13 +0100

    move base url outside function

--- src/Components/ShowReel/ShortCard/ShowReelImage.tsx
+++ src/Components/ShowReel/ShortCard/ShowReelImage.tsx
@@ -12,14 +12,18 @@ const ImageWrap = styled.div`
   height: 100%;
   padding: 0;
 `;
+const baseImgUrl = `http://files.nowhitetees.co.uk/homepage-thumbs/`;
 
 const ShowReelImage: React.FC<Props> = (props: Props) => {
-  const baseImgUrl = `https://www.nowhitetees.co.uk/img/homepage_thumbs/`;
-
   const { path, onClick } = props;
   return (
     <ImageWrap onClick={onClick}>
-      <Image src={`${baseImgUrl}${path}`} alt="show image" maxWidth="100%" />
+      <Image
+        src={`${baseImgUrl}${path}`}
+        alt="show image"
+        maxWidth="100%"
+        height="100%"
+      />
     </ImageWrap>
   );
 };
