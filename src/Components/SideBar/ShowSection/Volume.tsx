5a903408308fc1c0b124e7ec0cb05e44b23aacfb
Richard James <rich@Richards-MacBook-Pro.local>
2020-04-09 18:55:57 +0100

    update volume component to use vol from hook

--- src/Components/SideBar/ShowSection/Volume.tsx
+++ src/Components/SideBar/ShowSection/Volume.tsx
@@ -1,24 +1,55 @@
 import React from "react";
 import styled from "styled-components/macro";
-// import { Howler } from "howler";
+import { useAudioPlayer } from "react-use-audio-player";
+import { VolumeMute, Volume as VolumeIcon } from "grommet-icons";
 
+import { buttonColour, Button } from "../../../GlobalDefinitions";
 import { SideBarContainer } from "./SideBarDefinitions";
-
-// out of use until react-use-audio-player exposes volume control
 
 const Container = styled(SideBarContainer)``;
 
+const ControlRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const VolumeSlider = styled.input`
+  width: 80%;
+`;
+
 const Volume = () => {
+  const { volume, mute } = useAudioPlayer();
+  const [isMuted, setMuted] = React.useState(false);
+
+  const setMute = () => {
+    if (!isMuted) {
+      setMuted(true);
+      mute();
+    } else {
+      setMuted(false);
+      mute();
+    }
+  };
+
+  const muteButton = (isMuted: boolean) =>
+    isMuted ? (
+      <VolumeMute color={buttonColour} />
+    ) : (
+      <VolumeIcon color={buttonColour} />
+    );
+
   return (
     <Container height={15} width={100}>
-      <label htmlFor="volume">Volume</label>
-      <input
-        type="range"
-        min={0}
-        max={1}
-        id="volume"
-        // onChange={({ target }) => Howler.volume(+target.value)}
-      />
+      <ControlRow>
+        <Button onClick={() => setMute()}>{muteButton(isMuted)}</Button>
+        <VolumeSlider
+          type="range"
+          min={0}
+          max={1}
+          id="volume"
+          onChange={({ target }) => volume(+target.value)}
+        />
+      </ControlRow>
     </Container>
   );
 };
