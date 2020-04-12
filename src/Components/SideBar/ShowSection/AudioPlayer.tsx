707e8792d875d2c15eade524ac04c91c4bdfb51f
Richard James <rich@Richards-MacBook-Pro.local>
2020-04-12 17:50:16 +0100

    seperate audioPlayer and PlaybackButtons

--- src/Components/SideBar/ShowSection/AudioPlayer.tsx
+++ src/Components/SideBar/ShowSection/AudioPlayer.tsx
@@ -1,79 +1,20 @@
 import React from "react";
-import styled from "styled-components/macro";
-import { useAudioPlayer, useAudioPosition } from "react-use-audio-player";
-import {
-  PlayFill,
-  PauseFill,
-  StopFill,
-  ForwardTen,
-  BackTen
-} from "grommet-icons";
-
-import { buttonsHeight, SideBarContainer } from "./SideBarDefinitions";
-import { buttonColour, Button } from "../../../GlobalDefinitions";
-
-const Container = styled(SideBarContainer)`
-  padding-left: 7.5%;
-  padding-right: 7.5%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
+import { useAudioPlayer } from "react-use-audio-player";
 
 interface IProps {
   file: string;
 }
 
-declare type BackOrForth = "back" | "forth";
-
 const urlPrefix = `https://nowhitetees.co.uk/recordings/`;
 
-const AudioPlayer: React.FC<IProps> = ({ file }) => {
-  const { playing, seek, stop, togglePlayPause } = useAudioPlayer({
+const AudioPlayer = (props: IProps) => {
+  const { file } = props;
+  useAudioPlayer({
     src: `${urlPrefix}${file}`,
-    format: "mp3"
+    format: "mp3",
+    autoplay: false
   });
-  const { position } = useAudioPosition();
-
-  const seek10Secs = (direction: BackOrForth) => {
-    if (direction === "forth") {
-      seek(position + 10);
-    } else {
-      seek(position - 10);
-    }
-  };
-
-  const playPauseButton = (isPlaying: boolean) =>
-    isPlaying ? (
-      <PauseFill color={buttonColour} />
-    ) : (
-      <PlayFill color={buttonColour} />
-    );
-
-  return (
-    <Container height={buttonsHeight} width={100}>
-      <Button
-        onClick={() => {
-          seek10Secs("back");
-        }}
-      >
-        <BackTen color={buttonColour} />
-      </Button>
-      <div>
-        <Button onClick={togglePlayPause}>{playPauseButton(playing)}</Button>
-        <Button onClick={() => stop()}>
-          <StopFill color={buttonColour} />
-        </Button>
-      </div>
-      <Button
-        onClick={() => {
-          seek10Secs("forth");
-        }}
-      >
-        <ForwardTen color={buttonColour} />
-      </Button>
-    </Container>
-  );
+  return <></>;
 };
 
 export default AudioPlayer;
---
+++ src/Components/SideBar/ShowSection/PlaybackButtons.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { useAudioPlayer, useAudioPosition } from "react-use-audio-player";
+import {
+  PlayFill,
+  PauseFill,
+  StopFill,
+  ForwardTen,
+  BackTen
+} from "grommet-icons";
+
+import { buttonsHeight, SideBarContainer } from "./SideBarDefinitions";
+import { buttonColour, Button } from "../../../GlobalDefinitions";
+
+const Container = styled(SideBarContainer)`
+  padding-left: 7.5%;
+  padding-right: 7.5%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+declare type BackOrForth = "back" | "forth";
+
+const PlaybackButtons = () => {
+  const { playing, seek, stop, togglePlayPause } = useAudioPlayer();
+  const { position } = useAudioPosition();
+
+  const seek10Secs = (direction: BackOrForth) => {
+    if (direction === "forth") {
+      seek(position + 10);
+    } else {
+      seek(position - 10);
+    }
+  };
+
+  const playPauseButton = (isPlaying: boolean) =>
+    isPlaying ? (
+      <PauseFill color={buttonColour} />
+    ) : (
+      <PlayFill color={buttonColour} />
+    );
+
+  return (
+    <Container height={buttonsHeight} width={100}>
+      <Button
+        onClick={() => {
+          seek10Secs("back");
+        }}
+      >
+        <BackTen color={buttonColour} />
+      </Button>
+      <div>
+        <Button onClick={togglePlayPause}>{playPauseButton(playing)}</Button>
+        <Button onClick={() => stop()}>
+          <StopFill color={buttonColour} />
+        </Button>
+      </div>
+      <Button
+        onClick={() => {
+          seek10Secs("forth");
+        }}
+      >
+        <ForwardTen color={buttonColour} />
+      </Button>
+    </Container>
+  );
+};
+
+export default PlaybackButtons;
