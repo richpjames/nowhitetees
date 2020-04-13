b804a539500b0946537f6b35079bb3ac30e1a9bf
Richard James <rich@Richards-MacBook-Pro.local>
2020-04-13 20:52:36 +0100

    add years and attempt correct positioning

--- src/Components/ShowReel/ShowReel.tsx
+++ src/Components/ShowReel/ShowReel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
-import { isSameDay } from "date-fns";
+import { isSameDay, differenceInCalendarYears, getYear } from "date-fns";
 
 import Show from "../../Models/Show";
 import Shows from "../../ShowsData";
@@ -12,19 +12,30 @@ const ShowReelWrap = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
+
+const YearTitle = styled.h2`
+  width: 100%;
+  margin-bottom: 25px;
+`;
+
 interface IProps {
   shows: Show[];
   className: string;
 }
 
+const shows = Shows.get();
+let evenSelected = false;
+
+const isInLeftColumn = (i: number, date: Date, prevShowDate: Date) => {
+  return !differenceInCalendarYears(prevShowDate, date) || evenSelected
+    ? i % 2 === 1
+    : i % 2 === 0;
+};
+
 const ShowReel: React.FC<IProps> = (props: IProps) => {
   const [expandedShow, setExpandedShow] = React.useState<Date>(new Date());
 
-  let evenSelected = false;
-  const isInLeftColumn = (i: number) =>
-    evenSelected ? i % 2 === 1 : i % 2 === 0;
-
-  const showReel = Shows.get().map((show, i) => {
+  const showReel = shows.map((show, i) => {
     const { date, id } = show;
     const selected = isSameDay(expandedShow, date);
 
@@ -32,15 +43,27 @@ const ShowReel: React.FC<IProps> = (props: IProps) => {
       evenSelected = i % 2 === 0 ? true : false;
     }
     return (
-      <ShowCard
-        onClick={setExpandedShow}
-        className="ShowCard"
-        date={date}
-        id={id}
-        key={id}
-        selected={selected}
-        rightMargin={isInLeftColumn(i)}
-      />
+      <>
+        <ShowCard
+          onClick={setExpandedShow}
+          className="ShowCard"
+          date={date}
+          id={id}
+          key={id}
+          selected={selected}
+          rightMargin={isInLeftColumn(
+            i,
+            date,
+            shows[i + 1] && shows[i + 1].date
+          )}
+        />
+        {shows[i + 1] &&
+          differenceInCalendarYears(shows[i + 1].date, date) === -1 && (
+            <>
+              <YearTitle>20{getYear(shows[i + 1].date)}</YearTitle>
+            </>
+          )}
+      </>
     );
   });
 
